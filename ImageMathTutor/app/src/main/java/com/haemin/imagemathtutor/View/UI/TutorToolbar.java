b23038947d07583770c37c03abb731e9ace99da9
haemin <goals001214@gmail.com>
2020-01-16 04:56:54 +0900

    -Add CustomView (TutorToolbar)

---
+++ ImageMathTutor/app/src/main/java/com/haemin/imagemathtutor/View/UI/TutorToolbar.java
@@ -0,0 +1,46 @@
+package com.haemin.imagemathtutor.View.UI;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.haemin.imagemathtutor.R;
+
+public class TutorToolbar extends Toolbar {
+
+    @BindView(R.id.btn_setting)
+    Button btnSetting;
+    @BindView(R.id.btn_notification)
+    Button btnNotification;
+
+    public TutorToolbar(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public TutorToolbar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public TutorToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+    void init(Context context){
+        View toolbar = LayoutInflater.from(context).inflate(R.layout.toolbar, this, false);
+        ButterKnife.bind(this,toolbar);
+
+        btnSetting.setOnClickListener(v -> {
+
+        });
+        btnNotification.setOnClickListener(v -> {
+
+        });
+    }
+}
