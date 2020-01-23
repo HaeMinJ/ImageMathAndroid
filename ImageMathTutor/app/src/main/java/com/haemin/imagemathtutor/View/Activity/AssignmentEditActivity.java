c5341d368c80e167befa2caffa263b005d93aef0
haemin <goals001214@gmail.com>
2020-01-24 03:59:14 +0900

    -Assignment Edit 화면 추가 (xml 작업 미완료)

---
+++ ImageMathTutor/app/src/main/java/com/haemin/imagemathtutor/View/Activity/AssignmentEditActivity.java
@@ -0,0 +1,14 @@
+package com.haemin.imagemathtutor.View.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.haemin.imagemathtutor.R;
+
+public class AssignmentEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment_edit);
+    }
+}
