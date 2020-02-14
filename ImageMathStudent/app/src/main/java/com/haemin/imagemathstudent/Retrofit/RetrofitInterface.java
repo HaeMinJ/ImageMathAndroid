d72113b7ef350669260889cf0512b9c3c99f7a0f
komh0086 <50826800+komh0086@users.noreply.github.com>
2020-02-14 16:49:30 +0900

    DB수정, 회원가입 버그수정 및 api추가

--- ImageMathStudent/app/src/main/java/com/haemin/imagemathstudent/Retrofit/RetrofitInterface.java
+++ ImageMathStudent/app/src/main/java/com/haemin/imagemathstudent/Retrofit/RetrofitInterface.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public interface RetrofitInterface {
 
-    @GET("/alarm")//알림리스트 출력 ok
+    @GET("/alarm")//수정 ok
     ArrayList<Alarm> getAlarmList(@Header("x-access-token") String accessToken);
 
     @GET("/notice")//수업에 해당하는 공지 출력 ok page구현
@@ -18,9 +18,8 @@ public interface RetrofitInterface {
 
     @Multipart
     @POST("/notice")//수정필요
-    void addNotice(@Header("x-access-token") String accessToken, @Field("name") String name, @Field("contents") String contents, @Part MultipartBody.Part noticeFiles);
+    void addNotice(@Header("x-access-token") String accessToken, @Field("title") String title, @Field("contents") String contents, @Part MultipartBody.Part noticeFiles);
     // imageFiles Parameter name : ?, normalFiles Parameter name : ?
-
 
     @GET("/account/school")//경로바꿈 ok
     ArrayList<School> getSchoolList();
@@ -28,7 +27,7 @@ public interface RetrofitInterface {
     @GET("/assignment")//ok
     ArrayList<Assignment> getAssignmentList(@Header("x-access-token") String accessToken, @Query("lectureSeq")String lectureSeq, @Query("page") int page);
 
-    @GET("/assignment/{assignmentSeq}")//assignment/{assignmentSeq}:1  이런식으로 경로작성 ok
+    @GET("/assignment/{assignmentSeq}")//assignment/1  이런식으로 경로작성 ok    내일 만들거 studentAssignment 필요함
     Assignment getAssignmentInfo(@Path("assignmentSeq") String assignmentSeq);
 
     @Multipart
@@ -37,48 +36,51 @@ public interface RetrofitInterface {
     // imageFile Parameter name : ?
 
     @GET("/lecture")//수업 리스트 출력 ok
-    ArrayList<Lecture> getLectureList(@Header("x-access-token") String accessToken, @Query("page") int page);
+    Call<ArrayList<Lecture>> getLectureList();
+
+    @GET("/lecture/student")//ok
+    Call<ArrayList<Lecture>> getMyLectureList(@Header("x-access-token")String accessToken, @Query("page") int page);
 
     @POST("/lecture/add")//경로바꿈 ok
     ArrayList<Lecture> addLecture(@Header("x-access-token") String accessToken, @FieldMap Map<String, String> lectureField);
     /*
     academySeq, name, weekDay, time, totalDate, week, studentNum, userType
      */
-//------------------------------------------------------------------------
-    @GET("/recognition/{lectureSeq}")// 보류
+    //-------------------------------------------
+    @GET("/lecture/recognition/{lectureSeq}")// ok mysql contain 경로바꿈
     ArrayList<User> getRequestUserList(@Header("x-access-token") String accessToken, @Path("lectureSeq") String lectureSeq, @Query("page") int page);
 
-    @PATCH("/recognition")//보류
-    void recognizeStudents(@Header("x-access-token") String accessToken, @Field("students") ArrayList<User> recognizedStudents, @Field("isAccept") boolean isAccept);
+    @PATCH("/lecture/recognition/{lectureSeq}")//보류
+    void recognizeStudents(@Header("x-access-token") String accessToken,@Path("lectureSeq")String lectureSeq, @Field("students") ArrayList<User> recognizedStudents, @Field("isAccept") boolean isAccept);
 
-    @GET("/student/{lectureSeq}")//보류
+    @GET("/lecture/student/{lectureSeq}")//ok  mysql contain
     ArrayList<User> getStudentList(@Header("x-access-token") String accessToken, @Path("lectureSeq") String lectureSeq);
-
-    @GET("/test/student/{lectureSeq}")
+//--------------------------------------------------
+    @GET("/test/student/{lectureSeq}")//ok testAdm userSeq, lectureSeq 해당 데이터리스트
     ArrayList<StudentTest> getUserTest(@Header("x-access-token") String accessToken, @Path("lectureSeq") String lectureSeq, @Query("page") int page);
 
-    @GET("/test/student")
+    @GET("/test/student")//ok testAdm accessToken 확인 ->userSeq testSeq 둘다 일치하는거score포함
     StudentTest getStudentTestInfo(@Header("x-access-token") String accessToken, @Query("testSeq") String testSeq);
 
-    @GET("/test/tutor/{lectureSeq}")
+    @GET("/test/tutor/{lectureSeq}")//ok testInfo
     ArrayList<Test> getTutorTestList(@Header("x-access-token")String accessToken, @Path("lectureSeq")String lectureSeq, @Query("page") int page);
-    @GET("/test/tutor")
+
+    @GET("/test/tutor")//ok testInfo score제외
     Test getTutorTestInfo(@Header("x-access-token") String accessToken, @Query("testSeq") String testSeq);
 
-    @GET("/test/result")
+    @GET("/test/result")//ok  testAdm score포함 accessToken -> student: 상위 10개 , tutor : 전부 다
     ArrayList<StudentTest> getTestResults(@Header("x-access-token")String accessToken, @Query("testSeq") String testSeq, @Query("page") int page);
 
-
-    @POST("/auth/register")
+    @POST("/auth/register")//ok
     User registerEmail(@FieldMap Map<String, String> registerField);
     /*
-    name , school~~~~~~~
+    name, eamil, password, birthday, schoolSeq, reqLedctureSeqs, userType, phone
      */
 
-    @GET("/auth/autologin")
+    @GET("/auth/autologin")//ok
     User loginWithToken(@Header("x-access-token") String accessToken);
 
-    @POST("/auth/login")
+    @POST("/auth/login")//ok
     User loginWithEmail(@Field("email") String email, @Field("password") String pw);
 
     /* 페이징 처리는 요청을 보낼때 0페이지면 1~20까지 데이터. 1페이지면 21~40 까지 데이터 */
